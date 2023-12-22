068c0d5843c5bc4c135acbf222acfe02f93f4f60
Muham <muhambriana.31@gmail.com>
2023-12-22 10:31:07 +0700

    add: new image, border image, font, list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tourism_app/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,64 +18,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 16.0),
-              child: const Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16.0),
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(children: [
-                    Icon(Icons.calendar_today),
-                    SizedBox(height: 8.0),
-                    Text('Open Everyday'),
-                  ],
-                  ),
-                  Column(children: [
-                    Icon(Icons.access_time_outlined),
-                    SizedBox(height: 8.0),
-                    Text('09.00 - 20.00')
-                  ],
-                  ),
-                  Column(children: [
-                    Icon(Icons.monetization_on),
-                    SizedBox(height: 8.0),
-                    Text('RP 25.000')
-                  ],
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              child: const Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
